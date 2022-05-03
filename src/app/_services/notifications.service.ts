15b6157f7549ae3e0f073561e94adeb93970528a
Babak <babak.jahanbani@yahoo.com>
2022-05-03 20:37:20 +0430

    notification service

--- src/app/_services/notifications.service.ts
+++ src/app/_services/notifications.service.ts
@@ -1,12 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
+interface Command {
+  id: number;
+  text?: string;
+  type: 'success' | 'error' | 'clear';
+}
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationsService {
-  constructor() {}
+  messages!: Subject<Command>;
+  constructor() {
+    this.messages = new Subject<any>();
+  }
 
-  add() {}
-  error() {}
-  clear() {}
+  addMessage(message: string) {
+    this.messages.next({
+      id: 1,
+      type: 'success',
+      text: message,
+    });
+  }
+  errorMessage(message: string) {
+    this.messages.next({
+      id: 2,
+      type: 'error',
+      text: message,
+    });
+  }
+  clearMessage(id: number) {
+    this.messages.next({
+      id: id,
+      type: 'clear',
+    });
+  }
 }
--- src/app/notifications/notification-list/notification-list.component.ts
+++ src/app/notifications/notification-list/notification-list.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationsService } from 'src/app/_services/notifications.service';
 
 @Component({
   selector: 'app-notification-list',
   templateUrl: './notification-list.component.html',
-  styleUrls: ['./notification-list.component.css']
+  styleUrls: ['./notification-list.component.css'],
 })
 export class NotificationListComponent implements OnInit {
+  constructor(private notificationsService: NotificationsService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
